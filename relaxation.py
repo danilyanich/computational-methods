7eeb53d19fa6e0d0e78d6018bd08b6ec859aa97f
danilyanich <danilyanich@gmail.com>
2016-12-14 03:02:12 +0300

    done

--- relaxation.py
+++ relaxation.py
@@ -8,23 +8,21 @@ def main():
 	with open('data/input.txt', mode='r') as file:
 		m = np.matrix(utils.parse(file))
 		f = np.matrix(utils.parse(file)).T
-	a, b = np.matrix(np.copy(m)), np.matrix(np.copy(f))
-	x, x_1, eps = np.array(f), np.array([0. for i in range(len(m))]), 1.0e-5
-	m = np.matrix(np.dot(m.T, m))
-	f = np.matrix(np.dot(m.T, f))
-	m = np.matrix(np.diag([1 for i in range(len(m))]) - m)
-	iteration, omega = 0, 1
-	while lg.norm(x - x_1, ord=np.inf) / omega >= eps:
+	a, b, eps = np.matrix(np.copy(m)), np.matrix(np.copy(f)), 1.0e-5
+	x, x_1 = np.array(f), np.array([0. for i in range(len(m))])
+	m_t = m.T
+	m = np.dot(m_t, m)
+	f = np.dot(m_t, f)
+	iteration, omega = 0, 1.5
+	while lg.norm(x - x_1, ord=np.inf) >= eps:
 		x_1 = np.copy(x)
 		for i in range(len(x)):
-			x[i] = (1 - omega) * x[i] - omega * (sum([m[i, j] * x[j] for j in range(len(x))]) + f[i]) / m[i, i]
+			x[i] -= omega * float(sum([x[j] * m[i, j] for j in range(len(x))]) - f[i]) / m[i, i]
 		iteration += 1
-		print(x)
-		pass
 	r = np.array(np.dot(a, x) - b).T[0]
 	with open('summary/relaxation.txt', mode='w+') as file:
 		file.write('x:')
-		[file.write('{:<15.5e}'.format(e)) for e in np.array(x.T)[0]]
+		[file.write('{:<15.5e}'.format(e)) for e in np.array(x).T[0]]
 		file.write('\nr:')
 		[file.write('{:<15.5e}'.format(e)) for e in r]
 		file.write('\n||r|| = {:e}'.format(lg.norm(r.T, ord=np.inf)))
